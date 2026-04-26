b226720aa9794a204db40bddcb21d2d356df1bb2
Muntadher Al-Hafidh <105360197+Alamiyaco@users.noreply.github.com>
2026-04-26 22:48:22 +0300

    config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
   ضع رابط Web App من Google Apps Script هنا بعد النشر.
 */
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycby9EltdpWNgWiDErNQ7JfxtWyBijTgGpPB-TIdG3mPUb85T2LWbEEUqmblXn2sKEfl8/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbw_o73JK2Aj9QpY8hQZQsuFWJUQL29UtjDzlgHS_XH5MLde9NKmx4RZbYHrTdIU2BXs/exec",
   CURRENCY: "$"
 };
